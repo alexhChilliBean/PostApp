7ada014a56e5eeeed8f4a7ee3625163d4bf84ece
Alex Holmes <alexholmes@alexs-mbp.home>
2021-09-27 17:12:53 +0100

    Tidied up code in line with code review

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+
+    validates :category, :content, :title, :author_name, presence: true
+    validates_inclusion_of :category, in: %w(Lifestyle Music Food Fitness)
 end
