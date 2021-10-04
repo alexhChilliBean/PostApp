2857144ad9ade0792b40c7be9f145febf4fe7ddf
Alex Holmes <alexholmes@alexs-mbp.home>
2021-10-04 12:17:52 +0100

    Seeded db

--- db/migrate/20211001145733_create_comments.rb
+++ db/migrate/20211001145733_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.text :content
+      t.string :name
       t.references :posts
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-c1 = Comment.create(content: "This blog is great", post_id: 26)
+c1 = Comment.create({content: "This blog is great"},{name:"Stevie Wonder"},{post_id: 26})
+c2 = Comment.create({content: "This blog sucks >:("},{name:"Steve"},{post_id: 26})
