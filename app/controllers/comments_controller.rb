ded62d072014736885886f2bdce390d78f16cd12
Alex Holmes <alexholmes@alexs-mbp.home>
2021-10-06 12:52:14 +0100

    Added validation to model, nested :comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,21 @@
 class CommentsController < ApplicationController
+    before_action :find_post
 
     def index
     end
     
     def new
+        
         @comment = Comment.new
     end
 
     def create
+        postID = params[:post_id]
         @comment = Comment.new(comment_params)
+        @comment.post_id = postID
         if @comment.save
             flash[:success] = "Comment Successfully Added"
-            return redirect_to post_path("#{comment_params[:post_id]}")
+            return redirect_to post_path("#{postID}")
         else
             flash[:error] = @comment.errors.full_messages
             render 'new'
@@ -23,4 +27,8 @@ class CommentsController < ApplicationController
         params.require(:comment).permit(:comment, :name, :content, :post_id)
     end
 
+    private
+    def find_post
+        @post = Post.find(params[:post_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  resources :posts, :comments
+  resources :posts do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
